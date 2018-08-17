289b811f507fd4c99a2a2151b4527347e9c4e061
lakshmithirupathammapathi <41730938+lakshmithirupathammapathi@users.noreply.github.com>
2018-08-17 10:37:09 +0530

    Create guvi28.java

---
+++ guvi28.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{ Scanner sc=new Scanner(System.in);
+		
+		int n=sc.nextInt();
+		
+int myArray[]={2,1,3};
+
+	for(int i=0; i<myArray.length; i++)
+{
+    System.out.println(myArray[i]+" - " + i);
+} 
+	}
+}
